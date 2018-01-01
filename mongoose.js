6e2dec37fd512364f9ba1ff07ac79d0315b67b80
Anubiella <rathvelcar@libero.it>
2018-01-01 19:44:58 +0100

    Add POST /todos route and refactor mongoose

---
+++ mongoose.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+module.exports = {mongoose}
+
+
+
+// newTodo.save().then((doc)=>{
+// 	console.log('Saved todo, ', doc);
+// }, (e)=>{
+// 	console.log('Unable to save todo..');
+// });
+
+// newUser.save().then((doc)=>{
+// 	console.log('Saved user ', doc);
+// }, (e)=>{
+// 	console.log('Unable to save user...');
+// });
